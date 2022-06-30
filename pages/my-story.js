bad19a8e15398753d6f71f238c7b2219bb94406e
songa1 <songachillethe1her@gmail.com>
2022-06-30 10:57:39 +0200

    Added google verification tag

--- pages/my-story.js
+++ pages/my-story.js
@@ -12,6 +12,7 @@ export default function MyStory() {
         <meta name="keywords" content="Cishahayo Songa Achille, Software Developer, Entrepreneur, Public Speaker, Content creator"></meta>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <meta name="google-site-verification" content="4Z07RV-1-Qub3aUIvRamziU_UKN2AZRnlyAORUy9tiA" />
       </Head>
       <div className="relative">
         <Header/>
