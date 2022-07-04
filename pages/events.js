f61d2ec137c80b0ed203a09598f1cd6c9e76cdf9
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-07-04 03:16:55 -0700

    Update events.js

--- pages/events.js
+++ pages/events.js
@@ -19,6 +19,9 @@ export default function Events(){
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
                 <link rel="manifest" href="/site.webmanifest"></link>
                 <meta name="google-site-verification" content="4Z07RV-1-Qub3aUIvRamziU_UKN2AZRnlyAORUy9tiA" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&display=swap" rel="stylesheet"></link>
             </Head>
             <div className="relative">
                 <Header/>
