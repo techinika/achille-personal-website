c125b01d65dfe8d7fc9ec8a881462b84c821f60d
songa1 <songachillethe1her@gmail.com>
2022-12-19 12:49:06 +0200

    Added skill sections

---
+++ data/skills.js
@@ -0,0 +1,44 @@
+
+
+export const skills = [
+    {
+        id: 1,
+        skill: "HTML",
+        level: 80
+    },
+    {
+        id: 2,
+        skill: "CSS",
+        level: 80
+    },
+    {
+        id: 3,
+        skill: "JavaScript Vanilla",
+        level: 75
+    },
+    {
+        id: 4,
+        skill: "Node JS",
+        level: 60
+    },
+    {
+        id: 5,
+        skill: "React JS & Next JS",
+        level: 90
+    },
+    {
+        id: 6,
+        skill: "PostgreSQL",
+        level: 80
+    },
+    {
+        id: 7,
+        skill: "Mongo DB",
+        level: 60
+    },
+    {
+        id: 8,
+        skill: "Firebase",
+        level: 80
+    }
+]
