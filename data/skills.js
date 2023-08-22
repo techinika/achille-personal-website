6da510a01ce7379bc620ea96d6f937278b971598
songa1 <songachillethe1her@gmail.com>
2023-08-22 17:54:21 +0200

    Made my website very simple

--- data/skills.js
+++ data/skills.js
@@ -1,44 +1,23 @@
-
-
 export const skills = [
-    {
-        id: 1,
-        skill: "HTML",
-        level: 80
-    },
-    {
-        id: 2,
-        skill: "CSS",
-        level: 80
-    },
-    {
-        id: 3,
-        skill: "JavaScript Vanilla",
-        level: 75
-    },
-    {
-        id: 4,
-        skill: "Node JS",
-        level: 60
-    },
-    {
-        id: 5,
-        skill: "React JS & Next JS",
-        level: 90
-    },
-    {
-        id: 6,
-        skill: "PostgreSQL",
-        level: 80
-    },
-    {
-        id: 7,
-        skill: "Mongo DB",
-        level: 60
-    },
-    {
-        id: 8,
-        skill: "Firebase",
-        level: 80
-    }
-]
+  {
+    id: 1,
+    skill: "Web Development (JavaScript/TypeScript - Full-Stack)",
+    level: 90,
+  },
+  {
+    id: 2,
+    skill: "Creating Contents (Writing Articles, Copywriting)",
+    level: 88,
+  },
+  {
+    id: 2,
+    skill:
+      "Entrepreneurship (Starting a startup, Managing a startup, Product Development)",
+    level: 71,
+  },
+  {
+    id: 3,
+    skill: "Public Speaking (Workshops, Lectures, Informative)",
+    level: 70,
+  },
+];
