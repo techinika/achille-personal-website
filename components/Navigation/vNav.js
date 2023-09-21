329476b2481cca5943d24bb0ab76b50a0bd202a9
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2023-09-21 22:42:28 +0200

    Update vNav.js and added the sessioninze link

--- components/Navigation/vNav.js
+++ components/Navigation/vNav.js
@@ -15,6 +15,9 @@ export default function VertNav() {
           <Link href="https://calendly.com/achille-songa/meet-achille-songa">
             <a target="_blank">Book a chat with me</a>
           </Link>
+          <Link href="https://sessionize.com/cishahayo-songa-achille/">
+            <a target="_blank">Speaking Sessions</a>
+          </Link>
         </div>
       </div>
     </>
