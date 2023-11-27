52564200ee2f806170fba9f6f972727e96d3e6a6
songa1 <songachillethe1her@gmail.com>
2023-11-27 11:54:39 +0200

    Added the experience data file

---
+++ data/experience.js
@@ -0,0 +1,146 @@
+export const experiences = [
+    {
+        id: 1,
+        experience: "Software Engineering",
+        companies: [
+            {
+                id: 1,
+                company: "Progress MiH",
+                website: "http://progressmih.com",
+                positions: [
+                    {
+                        id: 1,
+                        position: "Backend Developer",
+                        time: "August 2023 - Present",
+                        employment: "Full-time",
+                        technology: "JavaScript (Node JS), Apollo GraphQL, Python (Django), Docker, GitHub, VS Code",
+                        description: "I work with Frontend team to make API that are fit for our customers' requests."
+                    }
+                ],
+                projects: [
+                    {
+                        id: 1,
+                        project: "Transparency International Ifate Platform",
+                        link: "#"
+                    },
+                    {
+                        id: 2,
+                        project: "BOA Logistics",
+                        link: "#"
+                    }
+                ]
+            },
+            {
+                id: 2,
+                company: "Elabman Ltd",
+                website: "#",
+                positions: [
+                    {
+                        id: 1,
+                        position: "Frontend Developer",
+                        time: "May 2023 - October 2023",
+                        employment: "Contract",
+                        technology: "JavaScript (Node JS), Apollo GraphQL, Python (Django), Docker, GitHub, VS Code",
+                        description: "Implementing designs made by UI/UX designers to fulfill client's requirements. Emphasizing on usability and performance."
+                    }
+                ],
+                projects: [
+                    {
+                        id: 1,
+                        project: "RTB Curricula Platform",
+                        link: "#"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        experience: "Content Development",
+        companies: [
+            {
+                id: 2,
+                company: "Techinika.",
+                website: "http://techinika.co.rw",
+                positions: [
+                    {
+                        id: 1,
+                        position: "Content Developer",
+                        time: "June 2020 - Present",
+                        employment: "Part-time",
+                        technology: "WordPress Block Editor, Google Analytics, Google Search Console, Google Keyword Planner, YouTube, TubeBuddy",
+                        description: "I make contents that educate our audience on how technology works and how they can do different things using technology tools."
+                    }
+                ],
+                projects: [
+                    {
+                        id: 1,
+                        project: "Articles Written by me",
+                        link: "https://techinika.com/en/author/achille/"
+                    },
+                    {
+                        id: 2,
+                        project: "Videos made by me",
+                        link: "https://www.youtube.com/@techinika"
+                    }
+                ]
+            },
+            {
+                id: 2,
+                company: "My Personal Platforms",
+                website: "#",
+                positions: [
+                    {
+                        id: 1,
+                        position: "Blogger",
+                        time: "February 2023 - Present",
+                        employment: "Hobby",
+                        technology: "LinkedIn Newsletter",
+                        description: "I write about my personal experiences, lessons I learnt, and things I believe to be important or interesting to know about. I do this to keep my writing passion alive."
+                    },
+                    {
+                        id: 2,
+                        position: "Kinyarwanda Tutorials",
+                        time: "2020 - Present",
+                        employment: "Hobby",
+                        technology: "YouTube, Udemy, Google Workspace, Adobe Premiere Pro",
+                        description: "I enjoy making courses and tutorials that teach people how to speak my native language."
+                    },
+                    {
+                        id: 3,
+                        position: "Songs Lyrics",
+                        time: "Spetember 2019 - 2021",
+                        employment: "Hobby",
+                        technology: "YouTube, VSDC Video Editor, Filmora Video Editor, SocialBlade",
+                        description: "I was making lyrics videos of old school Rwandan songs that I love so much."
+                    },
+                    {
+                        id: 4,
+                        position: "Social Media Contents",
+                        time: "2018 - 2022",
+                        employment: "Hobby",
+                        technology: "Facebook",
+                        description: "I was making short form written contents based on what I believed and what I observed. This served as the source of experience to my writing."
+                    }
+                ],
+                projects: [
+                    {
+                        id: 1,
+                        project: "My LinkedIn Articles",
+                        link: "https://www.linkedin.com/newsletters/achille-songa-newsletter-7034443629926432768/"
+                    },
+                    {
+                        id: 2,
+                        project: "My Video Contents",
+                        link: "https://www.youtube.com/@achillesonga"
+                    },
+                    {
+                        id: 3,
+                        project: "My Social Media Contents",
+                        link: "https://web.facebook.com/search/top/?q=mindinmiddle&_rdr"
+                    }
+                ]
+            }
+        ]
+    },
+]
