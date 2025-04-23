c2f03e9ecbf8da69251d228f00347d43668f9bb1
songa1 <songachillethe1her@gmail.com>
2025-04-23 08:07:46 +0200

    Fix eslint issues

--- src/components/Forms/hireMe.tsx
+++ src/components/Forms/hireMe.tsx
@@ -2,19 +2,23 @@
 
 import React from "react";
 
-class HireMeForm extends React.Component {
-  render() {
-    return <div id="ff-compose"></div>;
-  }
-  componentDidMount() {
-    let script = document.createElement("script");
-    script.id = "ff-script";
-    script.src =
-      "https://formfacade.com/include/102297505509517785082/form/1FAIpQLScdc-NhhButE9imfWuCSetkRRkrVgh9FFtbnSfvZOwIoan_Qg/classic.js?div=ff-compose";
-    script.defer = true;
-    script.async = true;
-    document.body.appendChild(script);
-  }
-}
+// class HireMeForm extends React.Component {
+//   render() {
+//     return <div id="ff-compose"></div>;
+//   }
+//   componentDidMount() {
+//     let script = document.createElement("script");
+//     script.id = "ff-script";
+//     script.src =
+//       "https://formfacade.com/include/102297505509517785082/form/1FAIpQLScdc-NhhButE9imfWuCSetkRRkrVgh9FFtbnSfvZOwIoan_Qg/classic.js?div=ff-compose";
+//     script.defer = true;
+//     script.async = true;
+//     document.body.appendChild(script);
+//   }
+// }
+
+const HireMeForm = () => {
+  return <div></div>;
+};
 
 export default HireMeForm;
